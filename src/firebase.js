9977282c7fd491dd1d7b483bb774abf92cb19b16
AnmolPatil2 <anmol.patil004@gmail.com>
2019-04-28 02:26:08 +0530

    before redo

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+ import firebase from "firebase";
+ require("firebase/firestore");
+ 
+ 
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAw60hbpGZppOOt0_TNcTPgNdOTvfzhNtc",
+    authDomain: "startuptry1-fddd7.firebaseapp.com",
+    databaseURL: "https://startuptry1-fddd7.firebaseio.com",
+    projectId: "startuptry1-fddd7",
+    storageBucket: "startuptry1-fddd7.appspot.com",
+    messagingSenderId: "600706739987"
+  };
+ const fb= firebase.initializeApp(config);
+
+ const db=firebase.firestore();
+
+ export{fb,db};
--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Admin from "./views/Admin.vue";
+import Overview from "./views/Overview.vue";
+import Products from "./views/Products.vue";
+import Orders from "./views/Orders.vue";
+import {fb} from './firebase'
 
 Vue.use(Router);
 
-export default new Router({
+const router =  new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -14,7 +19,30 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
+      path: "/admin",
+      name: "admin",
+      component: Admin,
+      meta: { requiresAuth: true },
+      children:[
+        {
+          path: "overview",
+          name: "overview",
+          component: Overview
+        },
+        {
+          path: "products",
+          name: "products",
+          component: Products
+        },
+        {
+          path: "orders",
+          name: "orders",
+          component: Orders
+        }
+      ]
+    },
+    {
+      path: "about",
       name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
@@ -24,3 +52,19 @@ export default new Router({
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  const currentUser = fb.auth().currentUser
+
+  if (requiresAuth && !currentUser) {
+      next('/')
+  } else if (requiresAuth && currentUser) {
+      next()
+  } else {
+      next()
+  }
+})
+
+export default router;
