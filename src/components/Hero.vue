d7f1679e5e4ce7335e5022a7db5b060cf79adbb4
AnmolPatil2 <anmol.patil004@gmail.com>
2019-04-28 09:43:20 +0530

    git succefully connected

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -8,7 +8,7 @@
                   Vue Shop For Developers
                 </h1>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos numquam tempora, iure delectus totam minus quam aperiam ratione dolores magni voluptates ut necessitatibus odio ipsum fuga, voluptas ab praesentium nihil?
+                  git hub succesfull
                 </p>
                 <div class="hero-btn mt-5">
                     <button class="btn custom-btn btn-info mr-4">Explore</button>
