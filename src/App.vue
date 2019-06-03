29b2a11a2f5a8ecc18db7f51d0a53bdd50bc3f4a
AnmolPatil2 <anmol.patil004@gmail.com>
2019-06-03 09:53:54 +0530

    vuetify added

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <style lang="scss">
 #app {
-  font-family: 'Nunito', sans-serif;
+  font-family: "Nunito", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   // text-align: center;
